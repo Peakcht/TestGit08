d70b9e4e0a7cfe3db3b4c8ed877038c9d8896d5f
Chatchawat Kijsatharn <chatchawat_02@hotmail.com>
2024-02-05 11:38:27 +0700

    My-Nuxt3

---
+++ my-nuxt3/pages/space1.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="grid grid-cols-4 gap-2 px-2 py-2">
+  <div class = "border-solid border-4 ">01</div>
+  <div class = "border-solid border-4 row-span-2 col-span-2 pl-8 pt-4">02</div>
+  
+  <div class = "border-solid border-4 ">04</div>
+  <div class = "border-solid border-4 ">05</div>
+  
+  
+  <div class = "border-solid border-4 row-span-2 px-2 py-5">08 Hi, There is no such a magic thing like abcdd. </div>
+  <div class = "border-solid border-4 col-span-3 ">09</div>
+  
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-nuxt3/pages/table.vue
@@ -0,0 +1,32 @@
+<template>
+<table class="border-separate border-spacing-3 border border-slate-400 ...">
+  <thead>
+    <tr>
+      <th class="border border-slate-300 ...">State</th>
+      <th class="border border-slate-300 ...">City</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border border-slate-300 ...">Indiana</td>
+      <td class="border border-slate-300 ...">Indianapolis</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Ohio</td>
+      <td class="border border-slate-300 ...">Columbus</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Michigan</td>
+      <td class="border border-slate-300 ...">Detroit</td>
+    </tr>
+  </tbody>
+</table>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
